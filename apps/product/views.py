e9b951e548637e8ed02515e9c83c2f46aa8768e6
isko18 <toksonbaevislam2004@gmail.com>
2025-03-06 16:51:46 +0600

    lesson_4

--- apps/product/views.py
+++ apps/product/views.py
@@ -1,29 +1,40 @@
-from rest_framework.generics import CreateAPIView, ListAPIView, \
-DestroyAPIView, UpdateAPIView, RetrieveAPIView
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 from apps.product.models import Product
 from apps.product.serializer import ProductSerializer
 
-class ProductCreateAPIView(CreateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-class ProductListAPIView(ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-class ProductDetailAPIView(RetrieveAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    lookup_field = 'slug'
-
-class ProductUpdateAPIView(UpdateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    lookup_field = 'slug'
+class ProductMixins(GenericViewSet, 
+                    mixins.CreateModelMixin,
+                    mixins.ListModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin):
     
-
-class ProductDeleteAPIView(DestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    lookup_field = 'slug'
+    lookup_field = ("slug")
+
+
+# class ProductCreateAPIView(CreateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+# class ProductListAPIView(ListAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+# class ProductDetailAPIView(RetrieveAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     lookup_field = 'slug'
+
+# class ProductUpdateAPIView(UpdateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     lookup_field = 'slug'
+    
+# class ProductDeleteAPIView(DestroyAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     lookup_field = 'slug'
     
